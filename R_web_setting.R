153c911ca67f59f43b2c37fc2ce799a65541b041
jyao <jinyao@jyao.local>
2023-11-10 07:36:41 -0500

    Updated website

--- R_web_setting.R
+++ R_web_setting.R
@@ -1,14 +1,14 @@
 
 library("blogdown")
 
-blogdown::new_site()   # Build up the new website
-blogdown::new_site(theme = "roninro/hugo-theme-puppet")
+# blogdown::new_site()   # Build up the new website
+# blogdown::new_site(theme = "roninro/hugo-theme-puppet")
 blogdown::serve_site() # Preview the website
 
 blogdown::stop_server()
 
 # Change the theme
-blogdown::install_theme(theme = "roninro/hugo-theme-puppet", force = TRUE)
+# blogdown::install_theme(theme = "roninro/hugo-theme-puppet", force = TRUE)
 blogdown::build_site()
 
 
